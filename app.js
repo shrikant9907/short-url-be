28cbbedccf041006a11be5d6d3eea782b896b787
Shrikant Yadav <shrikant9907@gmail.com>
2024-11-21 11:38:46 +0530

    node js server configuration

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const hostname = process.env.HOSTNAME ?? "localhost"; // 127.0.0.0
+const port = process.env.PORT ?? "5000";
+
+// Middlewares
+
+// Routes 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Server Start Process
+app.listen(port, () => {
+    console.log(`Server running at http://${hostname}:${port}`)
+})
